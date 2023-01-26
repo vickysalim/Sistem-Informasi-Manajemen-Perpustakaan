45131e1c5ae7e32dac78914cbe287808347f5e30
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-01-26 22:09:02 +0700

    install laravel breeze & config dashboard view

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('admin'),
+            'role' => 'ADMIN'
+        ]);
+
+    }
+}
---
+++ resources/views/pages/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('title', 'Beranda')
+
+@section('content')
+    Test
+@endsection
