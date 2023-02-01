aa53991f94c8f5bb36a9a6cc8c36b7bf24541608
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-02-01 21:14:10 +0700

    library db migrations

---
+++ database/migrations/2023_02_01_135624_create_circulations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('circulations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('member_id');
+            $table->bigInteger('book_id');
+            $table->date('loan_date');
+            $table->date('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('circulations');
+    }
+};
