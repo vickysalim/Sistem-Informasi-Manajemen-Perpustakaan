9500246332d05485990944560db603ce73857237
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-02-02 21:27:35 +0700

    initiate settings db

---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'key' => 'loan_duration',
+            'value' => '2'
+        ]);
+
+        Setting::create([
+            'key' => 'fine',
+            'value' => '2000'
+        ]);
+
+        Setting::create([
+            'key' => 'head_librarian',
+            'value' => 'Head Librarian Name'
+        ]);
+
+        Setting::create([
+            'key' => 'principal',
+            'value' => 'Principal Name'
+        ]);
+
+    }
+}
