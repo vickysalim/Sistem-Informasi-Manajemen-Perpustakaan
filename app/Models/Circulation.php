33e2a22fc9d4a24a11e467aa8df50a400512ce93
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-02-02 21:30:37 +0700

    update circulation db & model

--- app/Models/Circulation.php
+++ app/Models/Circulation.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Circulation extends Model
 {
     use HasFactory;
+
+    public function Member() {
+        return $this->belongsTo('App\Models\Member');
+    }
+
+    public function Book() {
+        return $this->belongsTo('App\Models\Book');
+    }
 }
