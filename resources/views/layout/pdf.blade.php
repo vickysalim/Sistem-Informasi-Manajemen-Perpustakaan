ceb2b077dfbf4a621fe9c727c43b25db15969d39
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-02-11 04:22:07 +0700

    create pdf page layout

---
+++ resources/views/layout/pdf.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        
+        <style>
+            body {
+                font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                font-size: 14px;
+                line-height: 1.42857143;
+                color: #333;
+                background-color: #fff;
+            }
+
+            h1, h2, h3, h4, h5, h6 {
+                text-align: center;
+                margin: 0;
+            }
+
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            .data-table {
+                margin-top: 32px;
+            }
+
+            .data-table th, .data-table td {
+                border: 1px solid;
+            }
+
+            .footer {
+                margin-top: 64px;
+            }
+
+            .footer th, .footer td {
+                padding: 6px 32px;
+                text-align: center;
+            }
+
+            .signature-space td {
+                padding-top: 64px;
+            }
+
+            .text-center {
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
